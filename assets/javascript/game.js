878be7d5d4e1f1abfa1cb9ab662a1c5eb5fe0e64
Jonathan Plata <jonathan_plata@ymail.com>
2017-08-25 18:52:42 -0700

    still working on battle game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,8 @@ var heroLetter;
 var heroSelected = false;
 var enemySelected = false;
 var hero;
+var enemy;
+var enemyDefeated = [];
 
 var heroA = {
 	health: 100,
@@ -15,6 +17,25 @@ var heroB = {
 var enemyA = {
 	health: 100,
 	counterAttack: 5,
+	letter: 'a',
+}
+
+var enemyB = {
+	health: 160,
+	counterAttack: 4,
+	letter: 'b',
+}
+
+var enemyC = {
+	health: 80,
+	counterAttack: 15,
+	letter: 'c',
+}
+
+var enemyD = {
+	health: 150,
+	counterAttack: 12,
+	letter: 'd',
 }
 
 $(document).on("click","img", function() {
@@ -30,25 +51,28 @@ $(document).on("click","img", function() {
 	}
 
 })
-
+//Attack Button 
 $(document).on("click", "h4", function() {
-
 	var hpBar = $(".enemyHp");
-	var objClass = $(this).attr("class");
 
-	if(objClass == "attackButton") {
-		alert("btn works");
-		enemyA.health -= hero.attack;
+	if($(this).attr("class") == "attackButton") {
+		enemy.health -= hero.attack;
 		hero.attack += 5;
 
-		hpBar.animate({width: enemyA.health + 'px'}, "show");
+		hpBar.animate({width: enemy.health + 'px'}, "show");
 
-		alert(enemyA.health);
-		alert(hero.attack);
+		console.log("Enemy HP: " + enemy.health);
+		console.log("Hero Attack: "+ hero.attack);
+		enemyTurn();
+
+		if(enemy.health === 0) {
+			newEnemy(enemy.letter);
+		}
 	}
 })
 
 function setHero() {
+
 	if(heroLetter== 'a') {
 		hero = heroA;
 		$('.heroName').html("Hero A");
@@ -62,24 +86,50 @@ function setHero() {
 	} else if(heroLetter=='d') {
 
 	}
-
+	$(".heroHp").css("display", "initial");
+	$(".heroHp").animate({width: hero.health + 'px'}, "show");
+	$(".heroPicked").css("display","initial");
 	heroSelected = true;	
 }
 
 function setEnemy(e) {
-	if(e == 'a') {
-		$('.enemyName').html("Chips");
-		$('.enemyPicked').attr("src", "./assets/images/kingChip.jpg");
-	}else if(e == 'b') {
-		$('.enemyName').html("Candy");
-		// $('.enemyPicked').attr("src", "./assets/images/kingChip.jpg");
-	}else if(e == 'c') {
-		$('.enemyName').html("Ice Cream");
-		$('.enemyPicked').attr("src", "./assets/images/kingChip.jpg");
-	}else if(e == 'a') {
-		$('.enemyName').html("Chips");
-		$('.enemyPicked').attr("src", "./assets/images/kingChip.jpg");
-	}
 
+	var enemyCheck = enemyDefeated.indexOf(e);
+	if (enemyCheck === -1) {
+
+		if(e == 'a') {
+			enemy = enemyA;
+			$('.enemyName').html("Chips");
+			$('.enemyPicked').attr("src", "./assets/images/kingChip.jpg");
+		}else if(e == 'b') {
+			enemy = enemyB;
+			$('.enemyName').html("Candy");
+			$('.enemyPicked').attr("src", "./assets/images/kingChip.jpg");
+		}else if(e == 'c') {
+			enemy = enemyC;
+			$('.enemyName').html("Ice Cream");
+			$('.enemyPicked').attr("src", "./assets/images/kingChip.jpg");
+		}else if(e == 'a') {
+			enemy = enemyD;
+			$('.enemyName').html("Chips");
+			$('.enemyPicked').attr("src", "./assets/images/kingChip.jpg");
+		}
+	}
+	$(".enemyPicked").css("display","initial");
+	console.log("enemy is "+ enemy);
+
+	$(".enemyHp").css("display","initial");
+	$(".enemyHp").animate({width: enemy.health + 'px'}, "show");	
 	enemySelected = true;
+}
+
+function enemyTurn() {
+	var hpBar = $(".heroHp");
+	hero.health -= enemy.counterAttack;
+	hpBar.animate({width: hero.health + 'px'}, "show");
+}
+
+function newEnemy(letter) {
+	enemyDefeated.push(letter);
+
 }
